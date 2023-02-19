f42048c1d33fc5ae141d9bd5ec395a2e900f1a26
marwan19967 <marwanaly49@gmail.com>
2023-02-19 22:42:54 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './componets/first/first.component';
+import { SecondComponent } from './componets/second/second.component';
+import { AllInOneComponent } from './componets/day3/all-in-one/all-in-one.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SecondComponent,
+    AllInOneComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
+    /**
+     * 4- External Module [ HttpClientModule - RouterModule - FormsModule - ReactiveFormsModule ]
+     */
   ],
   providers: [],
   bootstrap: [AppComponent]
